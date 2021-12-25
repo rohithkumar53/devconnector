2e44889b03593bd3a707ba2726ce0942aec2da8c
rohithkumar53 <rohithkumar.pampari@gmail.com>
2021-12-25 00:33:28 +0000

    added initial routes

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const express=require('express');
+const express= require('express');
+const connectDB= require('./config/db');
 const app=express();
+
+//connect database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/',(req,res)=> res.send('API running'));
+app.use('/api/users', require("./routes/api/users"));
+app.use('/api/profile', require("./routes/api/profile"));
+app.use('/api/posts', require("./routes/api/posts"));
+app.use('/api/auth', require("./routes/api/auth"));
 
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
